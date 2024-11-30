a19a50dc1894157854dee20fa3948644bc014111
Ryota <rmiyagi@hal.ipc.i.u-tokyo.ac.jp>
2024-12-01 00:52:29 +0900

    fix github action

--- bindings/pybind_module.cpp
+++ bindings/pybind_module.cpp
@@ -4,8 +4,8 @@
 #include "base/PDAG.h"
 namespace py = pybind11;
 
-PYBIND11_MODULE(openbn, m) {
-    m.doc() = "OpenBN Python bindings";
+PYBIND11_MODULE(openbnsl, m) {
+    m.doc() = "OpenBNSL Python bindings";
     
     m.def("matmul_naive", &matmul_naive, "Multiply two NumPy arrays");
     m.def("matmul_openmp", &matmul_openmp, "Multiply two NumPy arrays with OpenMP");
--- test.sh
+++ test.sh
@@ -2,14 +2,14 @@
 
 PROJECT_ROOT=$(dirname "$(realpath "$0")")
 BUILD_DIR="$PROJECT_ROOT/build"
-TEST_EXEC="$BUILD_DIR/openbn_test"
+GTEST_EXEC="$BUILD_DIR/openbnsl_test"
 
 # Backend test (GoogleTest)
-if [ -x "$TEST_EXEC" ]; then
+if [ -x "$GTEST_EXEC" ]; then
     echo "Running GoogleTest..."
-    "$TEST_EXEC"
+    "$GTEST_EXEC"
 else
-    echo "Error: GoogleTest executable not found at $TEST_EXEC"
+    echo "Error: GoogleTest executable not found at $GTEST_EXEC"
     exit 1
 fi
 
