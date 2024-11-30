afbdebeee06ec16246ebae400551514a57df469c
Ryota <rmiyagi@hal.ipc.i.u-tokyo.ac.jp>
2024-11-30 12:29:16 +0900

    integrate pgmpy

--- include/base/PDAG.h
+++ include/base/PDAG.h
@@ -3,16 +3,16 @@
 #include <pybind11/numpy.h>
 namespace py = pybind11;
 
-class CPDAGwithAdjMat {
+class PDAGwithAdjMat {
 private:
     size_t n;
     std::vector<uint64_t> adj_mat;
 
 public:
-    CPDAGwithAdjMat(size_t n);
+    PDAGwithAdjMat(size_t n);
     bool has_edge(int from, int to);
-    add_edge(int from, int to);
-    remove_edge(int from, int to);
+    void add_edge(int from, int to);
+    void remove_edge(int from, int to);
     
     std::unordered_map<int, std::vector<int>> get_adj_list();
 };
---
+++ include/util.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <gmpxx.h>
+
+bool next_combination(mpz_class& combination, int n);
