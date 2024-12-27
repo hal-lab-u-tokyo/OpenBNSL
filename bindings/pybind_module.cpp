a1a6b988701f7cb6bfd5934855252549b1707fe9
nishikori3135 <nishikori-hirotaka2452@g.ecc.u-tokyo.ac.jp>
2024-12-27 18:50:53 +0900

    add RAI and PC on bindings/pybind_module.cpp

--- bindings/pybind_module.cpp
+++ bindings/pybind_module.cpp
@@ -2,6 +2,8 @@
 #include <pybind11/stl.h>
 
 #include "base/PDAG.h"
+#include "structure_learning/PC.h"
+#include "structure_learning/RAI.h"
 #include "template.h"
 namespace py = pybind11;
 
@@ -12,6 +14,8 @@ PYBIND11_MODULE(openbnsl, m) {
   m.def("matmul_naive", &matmul_naive, "Multiply two NumPy arrays");
   m.def("matmul_openmp", &matmul_openmp,
         "Multiply two NumPy arrays with OpenMP");
+  m.def("RAI", &RAI, "Run RAI algorithm");
+  m.def("PC", &PC, "Run PC algorithm");
 #ifdef USE_CUDA
   m.attr("cuda_enabled") = true;
   m.def("matmul_cuda", &matmul_cuda, "Multiply two NumPy arrays with CUDA");
