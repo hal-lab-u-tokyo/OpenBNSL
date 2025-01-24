40b00dc3d180ff2e7213682124224dda05aa69b8
nishikori3135 <nishikori-hirotaka2452@g.ecc.u-tokyo.ac.jp>
2025-01-24 15:45:20 +0900

    separate dataframe_wrapper from RAI_cpp.py

--- modules/RAI_cpp.py
+++ modules/RAI_cpp.py
@@ -9,37 +9,13 @@ from pgmpy.base import PDAG
 
 sys.path.append("/workspace")
 from modules.visualize_graph import display_graph_info as show
+from modules.dataframe_wrapper2 import dataframe_wrapper2
 
 
 def RAIEstimator_cpp(
     data, ESS, parallel=1, threshold_DP=0, search_neighbor=True, do_orientation_A2=True
 ):
-
-    # translate data from pandas dataframe to numpy array and extract column names
-    columns = np.array(data.columns.tolist())
-    t_data = data.to_numpy()
-    # print(columns)
-
-    # get statelist //重い
-    statelist = [[] for i in range(len(columns))]
-    for i in range(len(columns)):
-        for j in range(t_data.shape[0]):
-            if t_data[j][i] not in statelist[i]:
-                statelist[i].append(t_data[j][i])
-
-    # translate str matrix into int matrix and list of column names into list of int //重い
-    data_int = np.zeros(t_data.shape, dtype=np.uint8)
-    for i in range(t_data.shape[0]):
-        for j in range(len(columns)):
-            data_int[i][j] = statelist[j].index(t_data[i][j])
-    # print(data_int.dtype)
-    # print (t_data.dtype)
-    # data_int = openbnsl.str2int_numpy(t_data, statelist)
-
-    n_states = np.zeros(len(columns))
-    for i in range(len(columns)):
-        n_states[i] = len(statelist[i])
-    # print(n_states)
+    columns, data_int, n_states = dataframe_wrapper2(data)
 
     calc_time2 = 0
     t2 = time.perf_counter()
