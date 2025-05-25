9d54ac8ed5f1eb8bdc1297f860d1b2d9587f6929
Ryota <rmiyagi@hal.ipc.i.u-tokyo.ac.jp>
2025-05-25 15:29:15 +0900

    citest ok

--- include/base/contingency_table.h
+++ include/base/contingency_table.h
@@ -5,10 +5,10 @@
 #include "dataframe_wrapper.h"
 
 struct ContingencyTable {
-  std::vector<size_t> vars;
+  std::vector<size_t> var_ids;
   std::vector<size_t> cardinalities;
   std::vector<size_t> counts;
 };
 
-ContingencyTable buildContingencyTable(const std::vector<size_t>& vars,
+ContingencyTable buildContingencyTable(const std::vector<size_t>& var_ids,
                                        const DataframeWrapper& df);
--- src/base/contingency_table.cpp
+++ src/base/contingency_table.cpp
@@ -1,17 +1,17 @@
 #include "base/contingency_table.h"
 
-ContingencyTable buildContingencyTable(const std::vector<size_t>& vars,
+ContingencyTable buildContingencyTable(const std::vector<size_t>& var_ids,
                                        const DataframeWrapper& df) {
-  if (!std::is_sorted(vars.begin(), vars.end())) {
+  if (!std::is_sorted(var_ids.begin(), var_ids.end())) {
     throw std::invalid_argument("Input variable indices must be sorted.");
   }
 
   ContingencyTable ct;
-  ct.vars = vars;
+  ct.var_ids = var_ids;
 
   size_t totalCells = 1;
 
-  for (size_t v : vars) {
+  for (size_t v : var_ids) {
     size_t nvals = df.num_of_values[v];
     ct.cardinalities.push_back(nvals);
     totalCells *= nvals;
@@ -21,8 +21,8 @@ ContingencyTable buildContingencyTable(const std::vector<size_t>& vars,
   for (size_t i = 0; i < df.num_of_datapoints; i++) {
     size_t idx = 0;
     size_t multiplier = 1;
-    for (int j = static_cast<int>(vars.size()) - 1; j >= 0; j--) {
-      size_t v = vars[j];
+    for (int j = static_cast<int>(var_ids.size()) - 1; j >= 0; j--) {
+      size_t v = var_ids[j];
       idx += df.data_row_major[i][v] * multiplier;
       multiplier *= ct.cardinalities[j];
     }
