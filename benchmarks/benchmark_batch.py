80a22d61bad2953e7df4698bd06a8dca5e38f1a5
namekata <namekata-akira@g.ecc.u-tokyo.ac.jp>
2025-06-07 10:44:10 +0900

    benchmark (link)

--- benchmarks/benchmark_batch.py
+++ benchmarks/benchmark_batch.py
@@ -1,6 +1,7 @@
 import subprocess
 
-models = ["diabetes", "munin"]
+# models = ["diabetes", "munin"]
+models = ["link"]
 gpuPC_types = ["gpuPC", "gpuPC3"]
 citest_types = ["g2", "sc"]
 set_sizes = [20000, 40000, 100000, 200000, 400000]
@@ -11,4 +12,4 @@ for model in models:
       for set_size in set_sizes:
         subprocess.call("python3 -u benchmark_PC2.py "
         + gpuPC_type + " " + citest_type + " " + model + " " + str(set_size)
-        + " |tee -a benchmark_result.txt", shell=True)
+        + " |tee -a benchmark_result_link.txt", shell=True)
