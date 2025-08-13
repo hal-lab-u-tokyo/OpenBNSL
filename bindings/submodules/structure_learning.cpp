badd8acd32b435577343e5b9ac2a93bca4991362
Ryota <rmiyagi@hal.ipc.i.u-tokyo.ac.jp>
2025-08-13 12:07:44 +0900

    im sorry for removing nishikori work

--- bindings/submodules/structure_learning.cpp
+++ bindings/submodules/structure_learning.cpp
@@ -1,7 +1,5 @@
 #include <pybind11/pybind11.h>
 
-#include "structure_learning/PC_nishikori.h"
-#include "structure_learning/RAI_nishikori.h"
 #include "structure_learning/exhaustive_search.h"
 #include "structure_learning/pc.h"
 #include "structure_learning/rai.h"
@@ -11,8 +9,6 @@ void bind_structure_learning(py::module& m) {
   auto submodule =
       m.def_submodule("structure_learning", "Structure learning submodule");
 
-  submodule.def("PC_nishikori", &PC_nishikori, "Run PC Nishikori algorithm");
-  submodule.def("RAI_nishikori", &RAI_nishikori, "Run RAI Nishikori algorithm");
   submodule.def("exhaustive_search",
                 &exhaustive_search,
                 "Run exhaustive search algorithm",
