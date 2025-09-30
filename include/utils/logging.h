567d82647a1fb94557525d8b8f815bb93db628ad
Ryota Miyagi <rmiyagi@hal.ipc.i.u-tokyo.ac.jp>
2025-09-30 18:20:11 +0900

    add time logging

--- include/utils/logging.h
+++ include/utils/logging.h
@@ -1,10 +1,26 @@
 #pragma once
+#include <chrono>
+#include <ctime>
+#include <iomanip>
 #include <iostream>
 
-#define LOG_PRINT(level, stream_expr)                                        \
-  do {                                                                       \
-    std::cerr << "[" << level << "] " << __FILE__ << ":" << __LINE__ << " (" \
-              << __func__ << ") " << stream_expr << std::endl;               \
+inline std::string current_time_str() {
+  using namespace std::chrono;
+  auto now = system_clock::now();
+  auto t = system_clock::to_time_t(now);
+  auto ms = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
+
+  std::ostringstream oss;
+  oss << std::put_time(std::localtime(&t), "%Y-%m-%d %H:%M:%S") << "."
+      << std::setfill('0') << std::setw(3) << ms.count();
+  return oss.str();
+}
+
+#define LOG_PRINT(level, stream_expr)                                          \
+  do {                                                                         \
+    std::cerr << "[" << level << "][" << current_time_str() << "] " << __FILE__ \
+              << ":" << __LINE__ << " (" << __func__ << ") " << stream_expr    \
+              << std::endl;                                                    \
   } while (0)
 
 #ifdef ENABLE_DEBUG_LOG
